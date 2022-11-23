ae327ea6e648d06f7bd17a911f17071d1acd6544
Faiza Khan <56988513+faizarashid@users.noreply.github.com>
2022-11-23 12:40:57 +0500

    // Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with  places after the decimal.

---
+++ JsBasics/Day 3/HackerRank_2.js
@@ -0,0 +1,70 @@
+// Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with  places after the decimal.
+
+// Note: This challenge introduces precision problems. The test cases are scaled to six decimal places, though answers with absolute error of up to  are acceptable.
+
+// Example
+
+// There are  elements, two positive, two negative and one zero. Their ratios are ,  and . Results are printed as:
+
+// 0.400000
+// 0.400000
+// 0.200000
+// Function Description
+
+// Complete the plusMinus function in the editor below.
+
+// plusMinus has the following parameter(s):
+
+// int arr[n]: an array of integers
+
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'plusMinus' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function plusMinus(arr) {
+    // Write your code here
+    let count_zero=0,count_pos=0,count_neg=0;
+    
+    for(var i=0;i<=arr.length-1;i++){
+        if(arr[i]>0){count_pos++;}
+        else if(arr[i]<0){count_neg++;}
+        else {count_zero++;}
+    }
+    console.log((count_pos/arr.length).toFixed(6));
+    console.log((count_neg/arr.length).toFixed(6));
+      console.log((count_zero/arr.length).toFixed(6));
+
+}
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    plusMinus(arr);
+}
